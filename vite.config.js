99f1331659a4c444a2d72581a4e0bb32ff08cd3c
Dwi Santosa <dwiosa16@gmail.com>
2026-07-23 20:34:38 +0700

    Github Action

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue'; //import plugin vue
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/',
+  base: '/sociabuzzClonePortfolio/',
+  resolve: {
+    // Tambahkan alias untuk path yang lebih bersih
+    alias: [{ find: '@', replacement: '/src' }],
+  },
 });
