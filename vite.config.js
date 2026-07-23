2dc5ee868ab99812235ff2cb543122dbf139bf33
Dwi Santosa <dwiosa16@gmail.com>
2026-07-23 21:05:06 +0700

    nyoba build ulang lagi

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   // base: '/',
-  base: 'https://dwiosa16.github.io/sociabuzzClonePortfolio/',
+  base: '/sociabuzzClonePortfolio/',
   resolve: {
     // Tambahkan alias untuk path yang lebih bersih
     alias: [{ find: '@', replacement: '/src' }],
