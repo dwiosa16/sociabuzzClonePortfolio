ac40d1c331cfdd9cd54154c39918b26b46a08558
Dwi Santosa <dwiosa16@gmail.com>
2026-07-23 20:45:02 +0700

    pakai ini yang udah di ganti base urlnya

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/',
+  // base:'/',
+  base: '/sociabuzzClonePortfolio/',
   resolve: {
     // Tambahkan alias untuk path yang lebih bersih
     alias: [{ find: '@', replacement: '/src' }],
