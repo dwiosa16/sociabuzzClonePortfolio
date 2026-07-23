37aca916bf518a51aba78648151022e063ec9825
Dwi Santosa <dwiosa16@gmail.com>
2026-07-23 22:52:24 +0700

    fix: correct vite config for gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  // base: '/',
+  // Pastikan base URL sesuai dengan nama repository Anda di GitHub.
+  // Ini adalah path tempat situs Anda akan di-host.
+  // Contoh: https://<username>.github.io/<repository-name>/
+  // base:'/', //lokal
   base: '/sociabuzzClonePortfolio/',
+  plugins: [vue()],
   resolve: {
     // Tambahkan alias untuk path yang lebih bersih
     alias: [{ find: '@', replacement: '/src' }],
