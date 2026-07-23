fc23550d796b8229e364f4393ba83b6fba35da4b
Dwi Santosa <dwiosa16@gmail.com>
2026-07-23 20:54:20 +0700

    nyoba ganti base url https://dwiosa16.github.io/sociabuzzClonePortfolio/

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  // base:'/',
-  base: '/sociabuzzClonePortfolio/',
+  // base: '/',
+  base: 'https://dwiosa16.github.io/sociabuzzClonePortfolio/',
   resolve: {
     // Tambahkan alias untuk path yang lebih bersih
     alias: [{ find: '@', replacement: '/src' }],
